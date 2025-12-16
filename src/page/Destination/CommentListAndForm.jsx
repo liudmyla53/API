6d8d5762b8ebe1e958e11ac59126f4fba7bde611
liudmyla53 <lutik.dim@gmail.com>
2025-12-16 13:43:24 +0100

    Modifier CommentListAndForm

--- src/page/Destination/CommentListAndForm.jsx
+++ src/page/Destination/CommentListAndForm.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { postNewComment } from "../../services/DestinationService";
 
-export default function CommentListAndForm({ destinationId, initialComments }) {
-
-    // const [comments, setComments] = useState(initialComments);
+export default function CommentListAndForm({ destinationId, initialComments= []}) {
+  
+    const [comments, setComments] = useState(initialComments);
     const [author, setAuthor] = useState('');
     const [message, setMessage] = useState('');
 
@@ -12,29 +12,28 @@ export default function CommentListAndForm({ destinationId, initialComments }) {
         if (!author.trim() || !message.trim()) return;
         try {
             const newComment = await postNewComment(destinationId, author, message)
-            // setComments(prevComments => [...prevComments, newComment])
+            setComments(prevComments => [...prevComments, newComment])
             setAuthor('');
             setMessage('');
 
             //! Faire la requete AJAX Post pour envoyer le commentaire
         }
         catch (error) { console.error('Erreur lors de lenvoi du commentaire:', error) }
-
     }
     return (
         <>
-            {/* <h3>Commentaire: {comments.length}</h3>
+            <h3>Commentaire: {comments.length}</h3>
             {
                 comments.length === 0 ? (<p>Soyez le premier à commenter !</p>) : (
                     <ul>
                         {comments.map(c => (
                             <li key={c.id}>
-                                {c.author}: {c.text}
+                                {c.author}: {c.message}
                             </li>
                         ))}
                     </ul>
                 )
-            } */}
+            } 
             <h4>Laisser un commentaire</h4>
             <form onSubmit={handleSubmit}>
                 <input
